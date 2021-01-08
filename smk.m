95db4f363d985f7b70bb0c0bd5c98ff0b6b699b6
Kidserge <sorawit2000y2k@gmail.com>
2021-01-08 16:09:34 +0900

    Initial commit

---
+++ smk.m
@@ -0,0 +1,139 @@
+classdef smk
+    %SMK Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        is_start;
+        is_newdata;
+        
+        serial;
+        
+        iemg;
+        emg;
+        trigger;
+    end
+    
+    methods
+        function obj = smk(port)
+            %SMK Construct an instance of this class
+            %   Detailed explanation goes here
+            if nargin < 1   % no port given
+                ports = serialportlist
+                select = input("Select serial port for SMK EMG array device: ");
+                port = ports(select);
+            end
+            BAUDRATE = 460800;
+            byteorder = 'little-endian';
+            obj.serial = serialport(port, BAUDRATE, 'ByteOrder', byteorder, 'DataBits', 8);
+            obj.is_start = false;
+            obj.is_newdata = false;
+            obj.iemg = 0;
+            obj.emg = 0;
+            obj.trigger = 0;
+        end
+        
+        function obj = startIEMG(obj)
+            %"""
+            %intial iemg data transmittion from smk array system.
+            %return 1 if no problem found
+            %"""
+            %"AT+IEMGSTRT\r\n"
+            
+            if obj.serial.NumBytesAvailable > 0
+                read(obj.serial, obj.serial.NumBytesAvailable, "uint8");    %Empty memory
+            end
+            command = string(['AT+IEMGSTRT', char(13), newline]);
+            obj.sendCommand(command);
+            obj.is_start = true;
+            
+        end
+        
+        function obj = startEMG(obj)
+            %"""
+            %intial emg data transmittion from smk array system.
+            %return 1 if no problem found
+            %"""
+            %"AT+EMGSTRT\r\n"
+            
+            if obj.serial.NumBytesAvailable > 0
+                read(obj.serial, obj.serial.NumBytesAvailable, "uint8");    %Empty memory
+            end
+            command = string(['AT+EMGSTRT', char(13), newline]);
+            obj.sendCommand(command);
+            obj.is_start = true;
+        end
+        
+        function obj = stop(obj)
+            %METHOD1 Summary of this method goes here
+            %   Detailed explanation goes here
+            %"AT+STOP\r\n"
+            command = string(['AT+STOP', char(13), newline]);
+            obj.sendCommand(command);
+            obj.is_start = false;
+        end
+        
+        function max_setting(obj)
+            %METHOD1 Summary of this method goes here
+            %   Detailed explanation goes here
+            %"AT+EMGCONFIG=FFFFFFFF,2000\r\n"
+            command = string(['AT+EMGCONFIG=FFFFFFFF,2000', char(13), newline]);
+            obj.sendCommand(command);
+        end
+        
+        function obj_setting = check_setting(obj)
+            if obj.serial.NumBytesAvailable > 0
+                read(obj.serial, obj.serial.NumBytesAvailable, "uint8");    %Empty memory
+            end
+            
+            command = string(['AT+EMGCONFIG?', char(13), newline]);
+            obj.sendCommand(command);
+            pause(0.5);
+            
+            if obj.serial.NumBytesAvailable > 0
+                obj_setting = char(read(obj.serial, obj.serial.NumBytesAvailable, "uint8"));
+            else
+                obj_setting = "Setting do not received."
+            end
+        end
+        
+        function obj = loaddata(obj)
+            if obj.is_start == false
+                return
+            end
+            
+            start_byte = 0;
+            while start_byte ~= uint8(113) && start_byte ~= uint8(116)  %Loop to find start of emg (116) or iemg (113)
+                if obj.serial.NumBytesAvailable > 0
+                    start_byte = read(obj.serial, 1, "uint8");
+                end
+            end
+            
+            data_byte = read(obj.serial, 68, "uint8");
+            % 1 byte -> order 0 to 255
+            % 2 to 65 byte -> 2 bytes per data from 1 channel (32 channels)
+            % 66 to 68 byte -> 3 bytes for trigger
+            if start_byte == uint8(113)
+                obj.iemg = typecast(uint8(data_byte(2:65)), 'uint16');
+            elseif start_byte == uint8(116)
+                obj.emg = typecast(uint8(data_byte(2:65)), 'uint16');
+            end
+            obj.trigger = data_byte(66:68);
+            obj.is_newdata = true;
+        end
+        
+        function [obj, EMG, iEMG] = getEMG(obj)
+            
+            EMG = obj.emg;
+            iEMG = obj.iemg;
+            obj.is_newdata = false;
+            
+        end
+        
+        function sendCommand(obj, command)
+            %This function send command to the robot via buffer or serial
+            %port
+            writeline(obj.serial, command);
+        end
+    end
+end
+
---
+++ smk_example.m
@@ -0,0 +1,50 @@
+s = smk();
+% Enter 2 or serial port that connect to smk device here
+%2
+
+s.check_setting();
+% output should be
+% '00000001,250
+% 
+% OK
+% '
+% 00000001,250 -> transfer only 1 channel at 250 Hz
+pause(0.5);
+
+s = s.startIEMG();
+% Start transfer IEMG data, setting will NOT effect this
+pause(0.5);
+
+s = s.loaddata();
+% Load data into model keep call this to update the data
+pause(0.5);
+
+s = s.stop();
+% Stop transfer data
+pause(0.5);
+
+s.max_setting();
+% In order to transfer all EMG channel, max_setting is needed 
+pause(0.5);
+
+s.check_setting()
+% output should be
+% 'FFFFFFFF,2000
+% 
+% OK
+% '
+% FFFFFFFF,2000 -> transfer all 32 channel F -> 1111 mean 4 channel at
+% 2000 Hz
+pause(0.5);
+
+s = s.startEMG();
+% Start transfer EMG data, setting will effect this
+pause(0.5);
+
+s = s.loaddata();
+% Load data into model keep call this to update the data
+pause(0.5);
+
+s = s.stop();
+% Stop transfer data
+pause(0.5);
